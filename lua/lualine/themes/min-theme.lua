8d30a5c4a1e0fcc0030e228f435a2660af065aa6
datsfilipe <datsfilipe.foss@proton.me>
2023-09-04 18:59:27 -0300

    feat: add lualine theme

--- lua/lualine/themes/min-theme.lua
+++ lua/lualine/themes/min-theme.lua
@@ -5,45 +5,40 @@ local min = {}
 local bg = config.transparent and "NONE" or colors.bg
 
 min.normal = {
-	a = { bg = colors.blue, fg = colors.fgAlt, gui = "bold" },
+	a = { bg = colors.blue, fg = colors.bg, gui = "bold" },
 	b = { bg = bg, fg = colors.fg },
-	c = { bg = colors.blue, fg = colors.fgAlt },
+	c = { bg = bg, fg = colors.bg },
 }
 
 min.insert = {
-	a = { bg = colors.red, fg = colors.fgAlt, gui = "bold" },
+	a = { bg = colors.red, fg = colors.bg, gui = "bold" },
 	b = { bg = bg, fg = colors.fg },
-	c = { bg = colors.red, fg = colors.fgAlt },
 }
 
 min.terminal = {
-	a = { bg = colors.green, fg = colors.fgAlt, gui = "bold" },
+	a = { bg = colors.green, fg = colors.bg, gui = "bold" },
 	b = { bg = bg, fg = colors.fg },
-	c = { bg = colors.green, fg = colors.fgAlt },
 }
 
 min.command = {
-	a = { bg = colors.green, fg = colors.fgAlt, gui = "bold" },
+	a = { bg = colors.green, fg = colors.bg, gui = "bold" },
 	b = { bg = bg, fg = colors.fg },
-	c = { bg = colors.green, fg = colors.fgAlt },
 }
 
 min.visual = {
-	a = { bg = colors.purple, fg = colors.fgAlt, gui = "bold" },
+	a = { bg = colors.purple, fg = colors.bg, gui = "bold" },
 	b = { bg = bg, fg = colors.fg },
-	c = { bg = colors.purple, fg = colors.fgAlt },
 }
 
 min.replace = {
-	a = { bg = colors.orange, fg = colors.fgAlt, gui = "bold" },
+	a = { bg = colors.orange, fg = colors.bg, gui = "bold" },
 	b = { bg = bg, fg = colors.fg },
-	c = { bg = colors.orange, fg = colors.fgAlt },
 }
 
 min.inactive = {
-	a = { bg = bg, fg = colors.red },
-	b = { bg = bg, fg = colors.fg, gui = "bold" },
-	c = { bg = bg, fg = colors.red },
+	a = { bg = colors.fg, fg = colors.bg },
+	b = { bg = bg, fg = colors.orange, gui = "bold" },
+	c = { bg = bg, fg = colors.orange },
 }
 
 return min
