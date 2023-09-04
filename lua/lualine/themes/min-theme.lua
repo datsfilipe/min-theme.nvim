06e62782ed28afc6930d823ec6899afdcefe0e94
datsfilipe <datsfilipe.foss@proton.me>
2023-09-04 19:53:08 -0300

    refactor: change fg to comment in lualine

--- lua/lualine/themes/min-theme.lua
+++ lua/lualine/themes/min-theme.lua
@@ -6,37 +6,37 @@ local bg = config.transparent and "NONE" or colors.bg
 
 min.normal = {
 	a = { bg = colors.blue, fg = colors.bg, gui = "bold" },
-	b = { bg = bg, fg = colors.fg },
+	b = { bg = bg, fg = colors.comment },
 	c = { bg = bg, fg = colors.bg },
 }
 
 min.insert = {
 	a = { bg = colors.red, fg = colors.bg, gui = "bold" },
-	b = { bg = bg, fg = colors.fg },
+	b = { bg = bg, fg = colors.comment },
 }
 
 min.terminal = {
 	a = { bg = colors.green, fg = colors.bg, gui = "bold" },
-	b = { bg = bg, fg = colors.fg },
+	b = { bg = bg, fg = colors.comment },
 }
 
 min.command = {
 	a = { bg = colors.green, fg = colors.bg, gui = "bold" },
-	b = { bg = bg, fg = colors.fg },
+	b = { bg = bg, fg = colors.comment },
 }
 
 min.visual = {
 	a = { bg = colors.purple, fg = colors.bg, gui = "bold" },
-	b = { bg = bg, fg = colors.fg },
+	b = { bg = bg, fg = colors.comment },
 }
 
 min.replace = {
 	a = { bg = colors.orange, fg = colors.bg, gui = "bold" },
-	b = { bg = bg, fg = colors.fg },
+	b = { bg = bg, fg = colors.comment },
 }
 
 min.inactive = {
-	a = { bg = colors.fg, fg = colors.bg },
+	a = { bg = colors.comment, fg = colors.bg },
 	b = { bg = bg, fg = colors.orange, gui = "bold" },
 	c = { bg = bg, fg = colors.orange },
 }
