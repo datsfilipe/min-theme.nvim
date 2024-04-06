4693749822ef8f17fac54d6e9af203a132447b20
datsfilipe <datsfilipe.foss@proton.me>
2024-04-06 13:39:26 -0300

    refactor: update colors and custom highlights for js/ts
    - should look closer to Min theme now, at least for js/ts files.

--- after/queries/javascript/highlights.scm
+++ after/queries/javascript/highlights.scm
@@ -1,3 +1,14 @@
 ;; extends
 (pair
+  [
+    ";"
+    ","
+  ] @punctuation.terminator.statement
   ":" @punctuation.separator.keyvalue)
+(pair
+  ":" @punctuation.separator.keyvalue)
+[
+  (call_expression (identifier))
+  (member_expression (identifier))
+  (binary_expression (identifier))
+] @object.identifier
--- after/queries/typescript/highlights.scm
+++ after/queries/typescript/highlights.scm
@@ -1,5 +1,24 @@
 ;; extends
-(pair
-  ":" @punctuation.separator.keyvalue)
+
+[
+  (pair)
+    [
+      ";"
+      ","
+      "."
+    ] @punctuation.terminator.statement
+    ":" @punctuation.separator.keyvalue
+  (arguments)
+    [
+      ";"
+      ","
+      "."
+    ] @punctuation.terminator.statement
+]
 (type_annotation
   ":" @punctuation.separator.keyvalue)
+[
+  (call_expression (identifier))
+  (member_expression (identifier))
+  (binary_expression (identifier))
+] @object.identifier
