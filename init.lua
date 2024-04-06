99683dce61217b9c8f9771b59f22c3f327494aed
datsfilipe <datsfilipe.foss@proton.me>
2024-04-06 13:40:02 -0300

    refactor: update dev `init.lua` file

--- init.lua
+++ init.lua
@@ -1,3 +1,4 @@
+vim.opt.cursorline = true
 vim.opt.cmdheight = 1
 vim.opt.encoding = "utf-8"
 vim.scriptencoding = "utf-8"
@@ -41,6 +42,7 @@ vim.opt.backup = false
 vim.opt.undofile = true
 vim.opt.shell = "zsh"
 vim.opt.belloff = "all"
+vim.g.mapleader = ' '
 
 local lazypath = vim.fn.stdpath("data") .. "/development/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
@@ -59,17 +61,132 @@ require("lazy").setup({
 	root = vim.fn.stdpath("data") .. "/min/lazy",
 	spec = {
 		"nvim-telescope/telescope.nvim",
-		"lewis6991/gitsigns.nvim",
-		"lukas-reineke/indent-blankline.nvim",
+    { "lewis6991/gitsigns.nvim", opts = {} },
 		"nvim-lua/plenary.nvim",
-	},
+    {
+      "echasnovski/mini.hipatterns",
+      version = false,
+      event = 'VeryLazy',
+      config = function()
+        local hipatterns = require 'mini.hipatterns'
+
+        hipatterns.setup {
+          highlighters = {
+            hex_color = hipatterns.gen_highlighter.hex_color(),
+          }
+        }
+      end,
+    },
+    {
+      'nvim-treesitter/nvim-treesitter',
+      lazy = false,
+      config = function()
+        local ts = require 'nvim-treesitter.configs'
+
+        ts.setup {
+          highlight = {
+            enable = true,
+            disable = {},
+          },
+          indent = {
+            enable = true,
+          },
+          ensure_installed = {
+            'lua',
+            'vim',
+            'bash',
+            'javascript',
+            'typescript',
+            'tsx',
+            'html',
+            'css',
+            'json',
+            'go',
+            'rust',
+          },
+          autotag = {
+            enable = true,
+          },
+        }
+
+        local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
+        parser_config.tsx.filetype_to_parsername = { 'javascript', 'typescript.tsx' }
+      end,
+      build = function()
+        local ts_update = require('nvim-treesitter.install').update { with_sync = true }
+        ts_update()
+      end,
+      dependencies = {
+        {
+          'nvim-treesitter/nvim-treesitter-context',
+          opts = {},
+        },
+        'nvim-treesitter/playground',
+      },
+    },
+    {
+      'nvim-lualine/lualine.nvim',
+      event = 'VimEnter',
+      dependencies = { 'nvim-tree/nvim-web-devicons' },
+      opts = {
+        options = {
+          icons_enabled = true,
+          colorscheme = "min-theme",
+        },
+      },
+    },
+    {
+      'NeogitOrg/neogit',
+      cmd = 'Neogit',
+      dependencies = {
+        'nvim-lua/plenary.nvim',
+        'nvim-telescope/telescope.nvim',
+      },
+      opts = {},
+    },
+    {
+      'williamboman/mason-lspconfig.nvim',
+      event = { 'BufReadPre', 'BufNewFile' },
+      opts = {
+        ensure_installed = {
+          'tsserver'
+        },
+        automatic_installation = true,
+      },
+      dependencies = {
+        'williamboman/mason.nvim',
+        event = { 'BufReadPre', 'BufNewFile' },
+        build = ':MasonUpdate',
+        cmd = { 'Mason', 'MasonInstall', 'MasonUninstall', 'MasonUninstallAll', 'MasonUpdate', 'MasonLog' },
+        opts = {
+          log_level = vim.log.levels.WARN,
+          max_concurrent_installers = 4,
+        },
+      },
+    },
+  },
 })
+
+vim.keymap.set('i', '<C-c>', '<Esc>', opts)
+vim.keymap.set('n', '<leader>gg', ':Neogit<Return>', { noremap = true, silent = true })
+vim.keymap.set('n', '<leader>e', ':Explore<Return><Return>', opts)
+vim.keymap.set('v', '<', '<gv', opts)
+vim.keymap.set('v', '>', '>gv', opts)
+vim.keymap.set('n', '>', ':tabnext<Return>', opts)
+vim.keymap.set('n', '<', ':tabprev<Return>', opts)
+local theme = require('telescope.themes').get_dropdown {}
+vim.keymap.set('n', ';f', function()
+  require 'telescope.builtin'.find_files(vim.tbl_deep_extend('force', {
+    prompt_prefix = '   ',
+    no_ignore = false,
+    hidden = true,
+  }, theme))
+end, opts)
 
 local theme = require("min-theme/init")
 
 theme.setup({
-	theme = "dark",
-	transparent = true,
+	transparent = false,
 	italics = {
 		comments = false,
 		keywords = true,
